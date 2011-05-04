2228e44e05ad216e264b28bd2e269c8f9cdb9827
Hans Gutknecht <hansdotcom@gmail.com>
2011-05-04 11:57:33 -0400

    added tempalte files, reset and some other options

--- theme-settings.php
+++ theme-settings.php
@@ -1,9 +1,8 @@
 <?php
 function ultimate_mobile_form_system_theme_settings_alter(&$form, $form_state) {
-
   $form['grid_settings'] = array(
     '#type'          => 'fieldset',
-    '#title'         => t('Choose you settings'),
+    '#title'         => t('Choose your settings'),
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
   );
