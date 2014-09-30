a9d4c54f82ac57baafa30492d949a1e2570ce214
Hans Gutknecht <hans.gutknecht@mlssoccer.com>
2014-09-30 15:56:58 -0400

    Experiment with element query instead of window width
    Signed-off-by: Hans Gutknecht <hans.gutknecht@mlssoccer.com>

--- responsive_image_mapping.js
+++ responsive_image_mapping.js
@@ -12,7 +12,6 @@
     imgSwap: function() {
       var currentBreakpointMapping = ''
         , currentBreakpoint = 0
-        , breakpointCounter = 0
         , images = ''
         , currentImage = ''
         , imageLength = 0
@@ -20,22 +19,27 @@
         , newImgSource = ''
         , imageCount = 0
         , attrCount = 0
-        , placeholder = 0;
+        , placeholder = 0
+        , elementQueryWidth = '';
       // Check if we are using blank gif placeholders.
       placeholder = Drupal.settings.responsive_image_mapping.tiny_gif;
-      // Calculate current breakpoint.
-      currentBreakpoint = Drupal.behaviors.rimHelpers.calcBreakpoint();
-      if (currentBreakpoint >= 0) {
-        currentBreakpointMapping = 'data-responsive_image_mapping_' + currentBreakpoint;
-      }
-      else {
-        // If we do not match a break, fallback to default.
-        currentBreakpointMapping = 'data-responsive_image_mapping_preset';
-      }
       // Gather all responsive images.
       images = document.querySelectorAll('img.responsive_image_mapping');
       imageLength = images.length;
       for (imageCount = 0; imageCount < imageLength; imageCount++) {
+        currentImage = images[imageCount];
+        // Query the element container width.
+        // We are making the assumption of an <a> wrapper so we get the parent of that.
+        elementQueryWidth = $(currentImage).parent().parent().width();
+        // Set current breakpoint based on element query.
+        currentBreakpoint = Drupal.behaviors.rimHelpers.calcBreakpoint(elementQueryWidth);
+        if (currentBreakpoint >= 0) {
+          currentBreakpointMapping = 'data-responsive_image_mapping_' + currentBreakpoint;
+        }
+        else {
+          // If we do not match a break, fallback to default.
+          currentBreakpointMapping = 'data-responsive_image_mapping_preset';
+        }
         // If we are on the smallest breakpoint (data-responsive_image_mapping_0)
         // and are serving the smallest image no need to imgSwap.
         if (currentBreakpointMapping === 'data-responsive_image_mapping_0' && placeholder === 1) {
@@ -43,12 +47,8 @@
         }
         else {
           // Create the new image and swap in DOM.
-          currentImage = images[imageCount];
           newImgSource = currentImage.getAttribute(currentBreakpointMapping);
-          breakpointCounter = currentBreakpoint;
-          while (newImgSource === 'none') {
-            newImgSource = currentImage.getAttribute('data-responsive_image_mapping_' + (breakpointCounter++));
-          }
+          // Fallback to preset if not defined for some reason.
           if (newImgSource === null || newImgSource === '') {
             newImgSource = currentImage.getAttribute('data-responsive_image_mapping_preset');
           }
@@ -58,7 +58,7 @@
             var imgAttributes = currentImage.attributes;
             var imgAttributeLength = imgAttributes.length;
             for (attrCount = 0; attrCount < imgAttributeLength; attrCount++) {
-              if (imgAttributes[attrCount].name != 'height' && imgAttributes[attrCount].name != 'width' && imgAttributes[attrCount].name != 'src' && imgAttributes[attrCount].name != 'data-borealis-style' && imgAttributes[attrCount].name != 'data-locked') {
+              if (imgAttributes[attrCount].name != 'height' && imgAttributes[attrCount].name != 'width' && imgAttributes[attrCount].name != 'src' && imgAttributes[attrCount].name != 'data-locked' && (imgAttributes[attrCount].name).indexOf('data-responsive_image_mapping_')) {
                 newImg.setAttribute(imgAttributes[attrCount].name, imgAttributes[attrCount].value);
               }
             }
@@ -77,6 +77,7 @@
   Drupal.behaviors.rimHelpers = {
      /**
      * Calculate viewport height and width.
+     * Deprected, To be removed.
      * @return array
      */
     getViewport: function() {
@@ -94,9 +95,10 @@
     /**
      * Calculate active breakpoint.
      * -1 represents no break identified, use default.
+     * @param elementQueryWidth width in pixels of the images parent().parent()
      * @return int - index of active breakpoint.
      */
-    calcBreakpoint: function() {
+    calcBreakpoint: function(elementQueryWidth) {
       var breakpoints = ''
         , viewportSize = ''
         , previousBreak = 0
@@ -105,8 +107,7 @@
       breakpoints = Drupal.settings.responsive_image_mapping.mappings;
       breakpoints.forEach(function(breakpoint) {
         breakpoint = parseInt(breakpoint);
-        viewportSize = Drupal.behaviors.rimHelpers.getViewport();
-        if ((viewportSize.width > previousBreak) && (viewportSize.width < breakpoint)) {
+        if ((elementQueryWidth > previousBreak) && (elementQueryWidth < breakpoint)) {
           activeBreak = breakpointIndex;
         }
         previousBreak = breakpoint;
